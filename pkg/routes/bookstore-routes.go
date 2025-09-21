1d8283f38595715510359400fb5092072fe77318
ultranaveed <naveednovo01@gmail.com>
2025-09-22 00:27:32 +0600

    project start

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,8 @@
+package routes
+
+import "github.com/gorilla/mux"
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc()
+}
